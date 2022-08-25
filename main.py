9f23f2448ad76c3209c8fbaf04dd691bcf833fd6
Walking-S <88008899+Walking-S@users.noreply.github.com>
2022-08-25 23:02:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,13 +44,13 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 def get_day():
-  return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+  return time.strftime("%Y-%m-%d %A",time.localtime())
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},'days':{'value':get_day(),"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},'days':{'value':get_day()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 
 res = wm.send_template(user_id, template_id, data)
 print(res)
